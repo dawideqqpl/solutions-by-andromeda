112e6cc190599859adb4e5110b7e8852d9a4be88
dawideqqpl <dawidzielinski.programmer@gmail.com>
2025-10-23 22:37:08 +0200

    23.10.2025 v2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,15 +71,7 @@ const Contact = ({ onPolicyClick }) => { // Dodajemy 'onPolicyClick' jako prop
             </form>
           </div>
           
-          <div className="p-8 bg-brand-blue bg-opacity-40 rounded-lg">
-            <h3 className="text-2xl font-bold mb-6 text-center sm:text-left">Dane kontaktowe</h3>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 text-brand-muted">
-              <p className="flex items-center"><FaBuilding className="text-brand-orange mr-3 w-4" /> Andromeda Solutions Dawid Zieliński</p>
-              <p className="flex items-center"><FaFileAlt className="text-brand-orange mr-3 w-4" /> NIP: 6351870391</p>
-              <p className="flex items-center"><FaEnvelope className="text-brand-orange mr-3 w-4" /> dawidzielinski.programmer@gmail.com</p>
-              <p className="flex items-center"><FaPhone className="text-brand-orange mr-3 w-4" /> +48 572 072 278</p>
-            </div>
-          </div>
+      
         </div>
       </motion.div>
     </section>
